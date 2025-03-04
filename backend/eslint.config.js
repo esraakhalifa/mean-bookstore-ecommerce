25342a61d75fcc3e81f357fc238092ed8798c37b
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-03-04 22:33:32 +0200

    add eslint file

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,12 +1,31 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import {antfu} from '@antfu/eslint-config';
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+export default antfu({
+  rules: {'no-console': 'off'}
+}, {
+  files: ['**/*.ts', '**/*.js'],
+  rules: {
+    'style/max-statements-per-line': ['error', {max: 2}],
+    'style/object-curly-spacing': ['error', 'never'],
+    'style/brace-style': ['error', '1tbs', {allowSingleLine: true}]
+  }
+}, {
+  files: ['**/*.ts'],
+  rules: {curly: ['error', 'all']}
+}, {
+  files: ['**/*.js'],
+  rules: {
+    'style/max-statements-per-line': ['error', {max: 2}],
+    'style/semi': ['error', 'always', {omitLastInOneLineBlock: true}],
+    'style/comma-dangle': ['error', 'never'],
+    'style/operator-linebreak': ['warn', 'before'],
+    'style/arrow-parens': ['warn', 'always'],
+    'style/brace-style': ['error', '1tbs', {allowSingleLine: true}],
+    'prefer-template': 'warn',
+    'antfu/top-level-function': 'off',
+    'antfu/if-newline': 'off',
+    'test/prefer-lowercase-title': 'off',
+    'unused-imports/no-unused-vars': 'warn',
+    'curly': 'off'
+  }
+});
