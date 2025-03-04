1fcf9942d8f82e39e10d0fb85c167bbf7d09fcf1
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-03-04 15:08:26 +0200

    added the .env file and mongo uri

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO = process.env.MONGO_URI;
 
+app.use(cors());
 app.use(express.json());
 
 mongoose
@@ -31,6 +32,11 @@ app.get("/users", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
 
 /*app.post("/users", async (req, res) => {
   try {
