29ce0b8580bef825d98ddb99250e2a996f44027d
Esraa <esraakhalifa5@gmail.com>
2025-03-05 14:46:00 +0200

    feat: test Redis client for caching middleware

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import {createClient} from 'redis';
 
 dotenv.config();
 const app = express();
@@ -24,3 +25,42 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on  http://localhost:${PORT}`);
 });
+
+const client = createClient({
+  url: 'redis://localhost:6379' // Default Redis port
+});
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+async function connectRedis() {
+  await client.connect();
+  console.log('error here');
+}
+
+connectRedis();
+const data = {
+  id: '123',
+  title: 'Redis in Action',
+  author: 'Josiah L. Carlson',
+  price: '25.99'
+};
+const cacheData = async (data) => {
+  try {
+    await client.hSet(`user-session:${data.id}`, {
+      title: data.title,
+      author: data.author,
+      price: data.price
+    });
+
+    console.log('✅ Data cached in Redis');
+  } catch (err) {
+    console.error('❌ Error caching data:', err);
+  }
+};
+
+// const getcachedData = async () => {
+//   const allCache = await client.hGetAll('user-session:123');
+//   console.log(allCache);
+// };
+
+// getcachedData();
+cacheData(data);
