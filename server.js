6464de4242c1215400d9131cbdadcfe783982e0b
Esraa <esraakhalifa5@gmail.com>
2025-03-07 13:06:59 +0200

    feat: Add book caching middleware.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
 import {createClient} from 'redis';
+import {cacheBooksByPage, getCachedBooksByPage} from './middlewares/cache/bookCache.js';
 
 dotenv.config();
 const app = express();
@@ -26,41 +27,28 @@ app.listen(PORT, () => {
   console.log(`Server is running on  http://localhost:${PORT}`);
 });
 
-const client = createClient({
-  url: 'redis://localhost:6379' // Default Redis port
+const RedisClient = createClient({
+  url: 'redis://localhost:6379'
 });
-client.on('error', (err) => console.log('Redis Client Error', err));
+RedisClient.on('error', (err) => console.log('Redis Client Error', err));
 
 async function connectRedis() {
-  await client.connect();
-  console.log('error here');
+  await RedisClient.connect();
+  console.log('Redis Connected');
 }
-
+export {RedisClient};
 connectRedis();
-const data = {
-  id: '123',
-  title: 'Redis in Action',
-  author: 'Josiah L. Carlson',
-  price: '25.99'
-};
-const cacheData = async (data) => {
-  try {
-    await client.hSet(`user-session:${data.id}`, {
-      title: data.title,
-      author: data.author,
-      price: data.price
-    });
 
-    console.log('✅ Data cached in Redis');
-  } catch (err) {
-    console.error('❌ Error caching data:', err);
-  }
-};
+const dummyBooks = [
+  {_id: '1', title: 'Redis for Beginners', authors: ['John Doe'], price: 15.99, description: 'A beginner-friendly guide.', stock: 10, img: 'redis1.jpg'},
+  {_id: '2', title: 'Advanced Redis', authors: ['Jane Doe'], price: 25.99, description: 'Deep dive into Redis internals.', stock: 5, img: 'redis2.jpg'}
+];
 
-// const getcachedData = async () => {
-//   const allCache = await client.hGetAll('user-session:123');
-//   console.log(allCache);
-// };
-
-// getcachedData();
-cacheData(data);
+(async () => {
+  await cacheBooksByPage(1, dummyBooks);
+  const cachedBooks = await getCachedBooksByPage(1).catch((err) => {
+    console.log(err);
+  });
+  console.log('Cached Books:', cachedBooks);
+  // await client.quit(); // Close Redis connection
+})();
