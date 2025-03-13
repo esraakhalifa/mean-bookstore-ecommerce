2c718984bf4e6a2a17196f772840b1b83f6193b1
Basmala-Abdullah <basmala.abdullah.ahmed@gmail.com>
2025-03-13 11:47:43 +0200

    feat: add centralized error handling and request logging middleware

--- server.js
+++ server.js
@@ -5,15 +5,17 @@ import express from 'express';
 import session from 'express-session';
 import connectDB from './config/db.js';
 import passport from './config/passport.js';
+import errorHandler from './middlewares/errorHandler.js';
+import requestLogger from './middlewares/requestLogger.js';
 import router from './routes/index.js';
 
 dotenv.config();
 const app = express();
+app.use(requestLogger);
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(router);
 
 connectDB();
 
@@ -30,6 +32,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(router);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
